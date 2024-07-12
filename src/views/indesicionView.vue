f0a4a2f8305823633952a68475609249335fea8a
Carmelo-Gambale <cgambale@prodtech.com.ar>
2024-07-12 10:48:04 -0300

    Seccion 5

--- src/views/indesicionView.vue
+++ src/views/indesicionView.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import ChatMessage from '@/components/chat/chatMessage.vue'
-import messageBox from '@/components/chat/messageBox.vue'
-</script>
 
 <template>
   <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
@@ -10,10 +6,45 @@ import messageBox from '@/components/chat/messageBox.vue'
     </div>
 
     <!-- chatmessage -->
-    <ChatMessage />
+    <ChatMessage :messages="messages"/>
 
     <!-- MessageBox -->
 
     <messageBox />
   </div>
 </template>
+
+<script setup lang="ts">
+import ChatMessage from '@/components/chat/chatMessage.vue'
+import messageBox from '@/components/chat/messageBox.vue'
+import type { ChatMessages } from '@/interfaces/chat-message.interface';
+import { ref } from 'vue';
+
+
+
+
+const messages = ref<ChatMessages[]>([
+
+{
+  id:new Date().getTime(),
+  message: 'Hola mundo',
+  itsMine:true,
+},
+{
+  id:new Date().getTime() +1,
+  message: '¿Queres salir a tomar un cafe?',
+  itsMine:true,
+},
+{
+  id:new Date().getTime(),
+  message: 'Hola mundo',
+  itsMine:false,
+  image:"https://yesno.wtf/assets/no/6-4bf0a784c173f70a0cab96efd9ff80c9.gif"
+},
+
+
+
+]);
+
+</script>
+
